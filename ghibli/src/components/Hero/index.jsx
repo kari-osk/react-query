c2aa3db06301c2b61a39be16aee2d9eb15ff2022
kari-osk <karinaosuka@gmail.com>
2022-09-08 21:10:07 -0300

    Add function in hero component

--- ghibli/src/components/Hero/index.jsx
+++ ghibli/src/components/Hero/index.jsx
@@ -3,6 +3,11 @@ import { Container, Row, Col } from 'react-bootstrap'
 import './style.css'
 
 function Hero() {
+
+  function scrollToComponent(id) {
+    document.getElementById(id).scrollIntoView();
+  }
+
   return (
     <div className='hero'>
       <Container>
@@ -10,7 +15,7 @@ function Hero() {
           <Col sm={12} md={6} lg={6} xl={6} className='hero_text'>
             <h1>Studio Ghibli</h1>
             <p>Cofundado por Hayao Miyazaki, o Studio Ghibli é famoso por suas produções fascinantes.</p>
-            <button className='hero_btn'>Veja mais</button>
+            <button className='hero_btn' onClick={() => scrollToComponent('films')} >Veja mais</button>
           </Col>
         </Row>
       </Container>
