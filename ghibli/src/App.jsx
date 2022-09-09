a89a06b57a49cd1d23ad54291d16ba0f91cd4f10
kari-osk <karinaosuka@gmail.com>
2022-09-08 21:12:22 -0300

    Alter style

--- ghibli/src/App.jsx
+++ ghibli/src/App.jsx
@@ -1,8 +1,6 @@
 import { QueryClientProvider, QueryClient } from 'react-query'
 import Gallery from './components/Gallery';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 
